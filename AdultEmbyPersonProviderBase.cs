e6b56ff5e31ee265113548c171f68817cfd8fab2
AdultEmby <adultemby@users.noreply.github.com>
2017-12-01 20:58:14 +0000

    Update for refactored birthPlace field in Emby model

--- AdultEmbyPersonProviderBase.cs
+++ AdultEmbyPersonProviderBase.cs
@@ -225,7 +225,6 @@ namespace AdultEmby.Plugins.Base
 
                 if (!string.IsNullOrWhiteSpace(birthPlace))
                 {
-                    //item.PlaceOfBirth = birthPlace;
                     item.ProductionLocations = new string[] { birthPlace };
                 }
 
